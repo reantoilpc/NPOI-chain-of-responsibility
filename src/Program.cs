25e175e21b51338ce57597f0e01a405c8cfb2773
Ray <reantoilpc@gmail.com>
2020-11-15 13:00:38 +0800

    feat : create new project

---
+++ src/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            
+        }
+    }
+}
